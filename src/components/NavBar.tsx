326a187a66838b5291ddf409be1ad0945fa88fb3
Mina <MinaDawood>
2020-11-12 18:18:00 +0200

    End beta version

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,73 @@
+import React from "react";
+import { Box, Link, Flex, Button, Heading } from "@chakra-ui/core";
+import NextLink from "next/link";
+import { useLogoutMutation, useMeQuery } from "../generated/graphql";
+import { isServer } from "../utils/isServer";
+import { useRouter } from "next/router";
+
+interface NavBarProps {}
+
+export const NavBar: React.FC<NavBarProps> = ({}) => {
+  const router = useRouter();
+  const [{ fetching: logoutFetching }, logout] = useLogoutMutation();
+  const [{ data, fetching }] = useMeQuery({
+    pause: isServer(),
+  });
+
+  let body = null;
+
+  // data is loading
+  if (fetching) {
+    // user not logged in
+  } else if (!data?.me) {
+    body = (
+      <>
+        <NextLink href="/login">
+          <Link color="white" mr={2}>
+            Login
+          </Link>
+        </NextLink>
+        <NextLink href="/register">
+          <Link color="white" mr={2}>
+            Register
+          </Link>
+        </NextLink>
+      </>
+    );
+    // user is logged in
+  } else {
+    body = (
+      <Flex align="center">
+        <NextLink href="/create-post">
+          <Button as={Link} mr={4}>
+            Create Post
+          </Button>
+        </NextLink>
+        <Box mr={2}>{data.me.username}</Box>
+        <Button
+          onClick={async () => {
+            await logout();
+            router.reload();
+          }}
+          isLoading={logoutFetching}
+          variant="link"
+        >
+          Logout
+        </Button>
+      </Flex>
+    );
+  }
+
+  return (
+    <Flex zIndex={1} position="sticky" top={0} bg="tan" p={4} align="center">
+      <Flex flex={1} m="auto" align="center" maxW={800}>
+        <NextLink href="/">
+          <Link>
+            <Heading>MyReddit</Heading>
+          </Link>
+        </NextLink>
+        <Box ml={"auto"}>{body}</Box>
+      </Flex>
+    </Flex>
+  );
+};
